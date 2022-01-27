a949c862eecc96f2ab632e298909bfc2dfb978cd
metalabyss <colorful.hoshi@gmail.com>
2022-01-27 20:02:34 +0700

    Add search button to ActionBar.

--- app/src/main/java/io/farafonova/lotrquotebook/MainActivity.kt
+++ app/src/main/java/io/farafonova/lotrquotebook/MainActivity.kt
@@ -2,10 +2,27 @@ package io.farafonova.lotrquotebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val menuItemThatWasSelected: Int = item.itemId
+        if (menuItemThatWasSelected == R.id.action_search_character) {
+            Toast.makeText(applicationContext, "Search", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
